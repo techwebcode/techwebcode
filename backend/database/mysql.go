eecadcc56c53f90f84bd5d99a457f1a33a7601fb
Rajat Singh <rajat@izooto.com>
2026-08-14 02:35:27 +0530

    build images

--- backend/database/mysql.go
+++ backend/database/mysql.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/techwebcode/techwebcode/backend/config"
 
@@ -12,24 +13,43 @@ import (
 
 var DB *gorm.DB
 
+func getEnvWithDefault(key, defaultValue string) string {
+	if val := config.Get(key); val != "" {
+		return val
+	}
+	return defaultValue
+}
+
 func Connect() {
+	host := getEnvWithDefault("MYSQL_HOST", getEnvWithDefault("DB_HOST", "mysql"))
+	port := getEnvWithDefault("MYSQL_PORT", getEnvWithDefault("DB_PORT", "3306"))
+	user := getEnvWithDefault("MYSQL_USER", getEnvWithDefault("DB_USER", "techwebcode"))
+	pass := getEnvWithDefault("MYSQL_PASSWORD", getEnvWithDefault("DB_PASSWORD", "root@123"))
+	dbname := getEnvWithDefault("MYSQL_DATABASE", getEnvWithDefault("DB_NAME", "techwebcode"))
 
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		config.Get("MYSQL_USER"),
-		config.Get("MYSQL_PASSWORD"),
-		config.Get("MYSQL_HOST"),
-		config.Get("MYSQL_PORT"),
-		config.Get("MYSQL_DATABASE"),
+		user,
+		pass,
+		host,
+		port,
+		dbname,
 	)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	var db *gorm.DB
+	var err error
 
-	if err != nil {
-		log.Fatal(err)
+	// Retry connection up to 10 times to withstand MySQL startup delay
+	for i := 1; i <= 10; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			DB = db
+			log.Println("MySQL Connected successfully")
+			return
+		}
+		log.Printf("[Warning] Database connection attempt %d failed: %v. Retrying in 2 seconds...", i, err)
+		time.Sleep(2 * time.Second)
 	}
 
-	DB = db
-
-	log.Println("MySQL Connected")
+	log.Fatalf("[Error] Failed to connect to MySQL database after 10 attempts: %v", err)
 }
